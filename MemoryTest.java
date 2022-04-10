5881123b6d11cfa920175695623128c1b5f57d64
mihirmalladi42 <94694337+mihirmalladi42@users.noreply.github.com>
2022-04-10 12:23:55 -0400

    Update MemoryTest.java

--- MemoryTest.java
+++ MemoryTest.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 import java.util.Arrays;
 import java.util.Random;
 public class MemoryTest {
+    Colors colors = new Colors();
     public static int num = 0;
     public static int level = 1;
     public static void memory(int level) {
@@ -25,16 +26,20 @@ public class MemoryTest {
                          timer.cancel();
                          boolean checker = checker(symbols, setNum);
                          if (checker) {
-                            System.out.println("Correct.");
+                            System.out.println(Colors.GREEN + "Correct." + Colors.RESET);
                             boolean doAgain = doAgain(level);
                                 if (doAgain) {
                                     num = 0;
                                     memory(level+1);
                                     } else if (!doAgain) {
-                                    System.out.println("Thank you for testing your memory.");
+                                    System.out.println(Colors.CYAN + "Thank you for testing your memory." + Colors.RESET);
                                     }
                          } else if (!checker) {
-                             System.out.println("Incorrect. The correct order was " + Arrays.toString(symbols) + ".");
+                             String[] strSymbols = new String[symbols.length];
+                             for (int l = 0; l < symbols.length; l++) {
+                                 strSymbols[l] = Integer.toString(symbols[l]);
+                             }
+                             System.out.println(Colors.RED + "Incorrect. The correct order was '" + Colors.YELLOW + (String.join("",strSymbols)) + Colors.RED + "'." + Colors.RESET);
                          }   
                        }
                     }
@@ -43,17 +48,23 @@ public class MemoryTest {
     }
     public static boolean doAgain(int level) {
         Scanner nextLevel = new Scanner(System.in);
-        System.out.println("Do you want to go to level " + (level+1) + "? (Yes or no): ");
+        System.out.println(Colors.GREEN + "Do you want to go to level " + (level+1) + "? (Yes or no): " + Colors.RESET);
         String response = nextLevel.nextLine();
         if (response.equalsIgnoreCase("yes")) {
             return true;
+        } else if (response.equalsIgnoreCase("no")){
+            return false;
+        } else if (response.equalsIgnoreCase("schmetterling")){
+            System.out.println(Colors.YELLOW + "Ich freue mich, dass Sie an den Schmetterling gedacht haben, der im Krankenhaus war. Dies war jedoch eine Ja- oder Nein-Frage, also nehme ich dies als Nein." + Colors.RESET);
+            return false;
         } else {
+            System.out.println(Colors.RED + "Enter a correct response." + Colors.RESET);
             return false;
         }
     }
     public static boolean checker(int[] symbols, int setNum) {
         Scanner inOrder = new Scanner(System.in);
-        System.out.println("What were the numbers in order? (Type " + setNum + " after you type everything else) (IF YOU WERE ON LEVEL 1, JUST TYPE " + setNum + "): ");
+        System.out.println(Colors.BLUE + "What were the numbers in order? (Type " + setNum + " after you type everything else) (IF YOU WERE ON LEVEL 1, JUST TYPE " + setNum + "): " + Colors.RESET);
         String res = inOrder.nextLine();
         String[] resArr = res.split("");
         int[] newArrInt = new int[resArr.length];
@@ -66,7 +77,7 @@ public class MemoryTest {
                 return false;
             }
     }
-    public static void main(String[] args) {            
+    public static void main(String[] args) {        
         memory(level);
     }
 }
